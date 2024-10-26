2799c2143b4448f70d44bc96001a3c6ef68ee46f
kash2612 <kashgupta20@gmail.com>
2024-10-26 23:07:32 +0530

    specifying the source dir

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,4 +7,7 @@ export default defineConfig({
     host: true,  // This will expose it to the external network
     port: 5173,  // Default Vite port
   },
+  build: {
+    outDir: 'build',  // Set Vite to output to 'build' instead of 'dist'
+  },
 });
